011703a99567d5c452048ba9e568a364d9471d6f
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-21 14:28:19 +0300

    main-srceen: добавил обработчик клика и прокинул в него метод

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import App from './components/app/app';
 
 const init = () => {
   const offers = [`Beautiful &amp; luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
+  const showOffer = () => {
+    return `showOffer`;
+  };
 
   ReactDOM.render(
-      <App offers={offers}/>,
+      <App offers={offers} showOffer={showOffer}/>,
       document.getElementById(`root`)
   );
 };
