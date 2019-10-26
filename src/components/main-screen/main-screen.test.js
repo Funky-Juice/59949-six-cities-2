1fdd4fe18cb7e2c1b991481dcb51a224c3e06637
Funky-Juice <isa86box@gmail.com>
2019-10-26 22:13:21 +0300

    актуализировал старые снепшот-тесты и добавил новые

--- src/components/main-screen/main-screen.test.js
+++ src/components/main-screen/main-screen.test.js
@@ -1,9 +1,10 @@
 import renderer from 'react-test-renderer';
 import MainScreen from './main-screen';
 
+
 it(`MainScreen correctly renders`, () => {
   const tree = renderer
-    .create(<MainScreen offers={[`offer`]} showOffer={() => {}}/>)
+    .create(<MainScreen offers={[{}]}/>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
