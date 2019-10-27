88cf666309a342a066adac2bf2d854603ce5c778
Funky-Juice <isa86box@gmail.com>
2019-10-27 18:29:57 +0300

    добавил маршрут для страницы с детальной информацией по аренде

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,12 +3,22 @@ import {Fragment} from 'react';
 import MainScreen from '../main-screen/main-screen';
 import OfferDetails from '../offer-details/offer-details';
 
+
 const getPageScreen = (props) => {
-  switch (location.pathname) {
+  const path = location.pathname;
+  let offerId = null;
+  let offer = {};
+
+  if (path.includes(`offer-`)) {
+    offerId = path.substring(7);
+    offer = props.offers.find(({id}) => id === parseInt(offerId, 10));
+  }
+
+  switch (path) {
     case `/`:
       return <MainScreen offers={props.offers}/>;
-    case `/offer`:
-      return <OfferDetails/>;
+    case `/offer-${offerId}`:
+      return <OfferDetails offer={offer}/>;
     default:
       return <div style={{textAlign: `center`}}>
         <h1>404</h1>
