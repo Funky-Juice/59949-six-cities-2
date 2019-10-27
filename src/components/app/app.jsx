e720458a257675f417176a7086759a2a2341c5a1
Funky-Juice <isa86box@gmail.com>
2019-10-27 13:23:35 +0300

    добавил компонент с детальной информацией по предложению аренды(статика)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import {Fragment} from 'react';
 import MainScreen from '../main-screen/main-screen';
+import OfferDetails from '../offer-details/offer-details';
 
 const getPageScreen = (props) => {
   switch (location.pathname) {
     case `/`:
       return <MainScreen offers={props.offers}/>;
-    case `/second-page`:
-      return <h1>second-page</h1>;
+    case `/offer`:
+      return <OfferDetails/>;
     default:
       return <div style={{textAlign: `center`}}>
         <h1>404</h1>
