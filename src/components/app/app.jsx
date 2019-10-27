ecaab937981d1a869c3e4b307ac841d918bd1d22
Funky-Juice <isa86box@gmail.com>
2019-10-27 12:39:02 +0300

    имитация роутера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
+import {Fragment} from 'react';
 import MainScreen from '../main-screen/main-screen';
 
-const App = (props) => {
-  const {offers} = props;
-
-  return <MainScreen offers={offers}/>;
+const getPageScreen = (props) => {
+  switch (location.pathname) {
+    case `/`:
+      return <MainScreen offers={props.offers}/>;
+    case `/second-page`:
+      return <h1>second-page</h1>;
+    default:
+      return <div style={{textAlign: `center`}}>
+        <h1>404</h1>
+        <h1>Page not found</h1>
+      </div>;
+  }
 };
 
-App.propTypes = {
+const App = (props) => {
+  return <Fragment>{getPageScreen(props)}</Fragment>;
+};
+
+getPageScreen.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
