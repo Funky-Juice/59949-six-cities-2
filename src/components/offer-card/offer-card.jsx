d97d1745a86644234f2f9281162c795e2a89b8f4
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-28 18:30:21 +0300

    поправил компонент OfferCard

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types';
 
-const cardSpecialMark = (
-  <div className="place-card__mark">
-    <span>Premium</span>
-  </div>
-);
-
 const OfferCard = (props) => {
   const {offer, showOffer} = props;
 
   return <article className="cities__place-card place-card" onMouseEnter={() => (showOffer(offer))}>
-    {offer.isPremium ? cardSpecialMark : null}
+    {offer.isPremium && <div className="place-card__mark">
+      <span>Premium</span>
+    </div>}
 
     <div className="cities__image-wrapper place-card__image-wrapper">
       <a href="#">
@@ -23,7 +19,7 @@ const OfferCard = (props) => {
           <b className="place-card__price-value">&euro;{offer.price}</b>
           <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
-        <button className={`place-card__bookmark-button ${offer.isFavorite ? `place-card__bookmark-button--active` : null}  button`} type="button">
+        <button className={`place-card__bookmark-button ${offer.isFavorite ? `place-card__bookmark-button--active` : ``} button`} type="button">
           <svg className="place-card__bookmark-icon" width="18" height="19">
             <use xlinkHref="#icon-bookmark"></use>
           </svg>
