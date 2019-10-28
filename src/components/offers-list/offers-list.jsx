9b74bb6f1e043b842d57a0d39beb0fe919049e40
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-28 18:28:43 +0300

    переписал компонент OffersList

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -3,22 +3,6 @@ import PropTypes from 'prop-types';
 import OfferCard from '../offer-card/offer-card';
 
 class OffersList extends PureComponent {
-  static getScreen(props, showOffer) {
-    const {offers} = props;
-
-    return <section className="cities__places places">
-      <div className="cities__places-list places__list tabs__content">
-        {offers.map((offer, i) =>
-          <OfferCard
-            key={`offer-${i}-${offer.price}`}
-            offer={offer}
-            showOffer={showOffer}
-          />
-        )}
-      </div>
-    </section>;
-  }
-
   constructor(props) {
     super(props);
 
@@ -28,10 +12,26 @@ class OffersList extends PureComponent {
   }
 
   render() {
-    return OffersList.getScreen(this.props, (offer) => {
-      this.setState(() => {
-        return {offer};
-      });
+    return this._renderOffers(this.props.offers);
+  }
+
+  _renderOffers(offers) {
+    return <section className="cities__places places">
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer, i) =>
+          <OfferCard
+            key={`offer-${i}-${offer.price}`}
+            offer={offer}
+            showOffer={() => this._setOffer.call(this, offer)}
+          />
+        )}
+      </div>
+    </section>;
+  }
+
+  _setOffer(offer) {
+    this.setState(() => {
+      return {offer};
     });
   }
 }
