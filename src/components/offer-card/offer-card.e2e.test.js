bc29aba6efd00a29d9e8efd515a9b3e13c88785a
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-29 11:42:03 +0300

    актуализировал тесты

--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -2,6 +2,7 @@ import {shallow} from 'enzyme';
 import OfferCard from './offer-card';
 
 const mockOffer = {
+  id: 3,
   type: `Apartment`,
   price: 120,
   title: `Beautiful & luxurious apartment at great location`,
@@ -17,6 +18,7 @@ it(`OfferCard onMouseEnter returns offer object`, () => {
   const wrapper = shallow(<OfferCard
     offer={mockOffer}
     showOffer={showOfferCB}
+    getOfferDetails={() => {}}
   />);
 
   const offerCard = wrapper.find(`article.cities__place-card`);
@@ -25,4 +27,20 @@ it(`OfferCard onMouseEnter returns offer object`, () => {
 
   expect(showOfferCB).toHaveBeenCalledTimes(1);
   expect(showOfferCB.mock.calls[0][0]).toEqual(mockOffer);
+});
+
+it(`OfferCard title onClick returns offer id`, () => {
+  const getOfferDetailsCB = jest.fn();
+  const wrapper = shallow(<OfferCard
+    offer={mockOffer}
+    showOffer={() => {}}
+    getOfferDetails={getOfferDetailsCB}
+  />);
+
+  const offerCard = wrapper.find(`.place-card__name`);
+
+  offerCard.simulate(`click`);
+
+  expect(getOfferDetailsCB).toHaveBeenCalledTimes(1);
+  expect(getOfferDetailsCB.mock.calls[0][0]).toEqual(mockOffer.id);
 });
