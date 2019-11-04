1be43d221a73d7f6de8ef308c5945162b5e3ab9d
Funky-Juice <isa86box@gmail.com>
2019-11-04 16:49:33 +0300

    подробное описание объекта offer в propTypes

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -19,7 +19,29 @@ class Map extends PureComponent {
 }
 
 Map.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.object).isRequired
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    type: PropTypes.oneOf([`Apartment`, `Room`, `House`, `Hotel`]).isRequired,
+    price: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    features: PropTypes.arrayOf(PropTypes.string).isRequired,
+    items: PropTypes.arrayOf(PropTypes.string).isRequired,
+    rating: PropTypes.number.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+    description: PropTypes.string,
+    host: PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      status: PropTypes.string,
+    }).isRequired,
+    location: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired
+    }).isRequired
+  })).isRequired
 };
 
 export default Map;
