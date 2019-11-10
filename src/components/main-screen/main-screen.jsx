5644a7b6f79a8af86bf36d6c32dc17ba96628b90
Funky-Juice <isa86box@gmail.com>
2019-11-10 16:34:17 +0300

    создал компонент CitiesList(статика) и подключил на главную страницу

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,19 +1,26 @@
 import {offerPropTypes} from '../../prop-types/prop-types';
 import OffersList from '../offers-list/offers-list';
 import Map from '../map/map';
+import CitiesList from '../cities-list/cities-list';
 
 const MainScreen = (props) => {
   const {offers} = props;
 
-  return <div className="cities">
-    <div className="cities__places-container container">
-      <OffersList offers={offers}/>
+  return <>
+    <main className="page__main page__main--index">
+      <CitiesList/>
 
-      <div className="cities__right-section">
-        <Map offers={offers}/>
+      <div className="cities">
+        <div className="cities__places-container container">
+          <OffersList offers={offers}/>
+
+          <div className="cities__right-section">
+            <Map offers={offers}/>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>;
+    </main>
+  </>;
 };
 
 MainScreen.propTypes = {
