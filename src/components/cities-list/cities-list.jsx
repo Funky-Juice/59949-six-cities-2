82018f44cbe46c6e5ce3a861f0bff15cd4935718
Funky-Juice <isa86box@gmail.com>
2019-11-10 22:15:42 +0300

    переписал CitiesList как class; подключил store; запись в store offers для активного города;

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,34 +1,80 @@
 import {offerPropTypes} from '../../prop-types/prop-types';
+import {Component} from 'react';
+import {connect} from 'react-redux';
+import ActionCreator from '../../store/actions';
 
-const CitiesList = (props) => {
-  const {offers} = props;
-  const citiesList = [];
 
-  offers.map((offer) => citiesList.push(offer.city));
-  const filteredCitiesList = [...new Set(citiesList.map(JSON.stringify))].map(JSON.parse);
+export class CitiesList extends Component {
+  constructor(props) {
+    super(props);
 
-  return <>
-    <div className="tabs">
-      <section className="locations container">
-        <ul className="locations__list tabs__list">
-          {filteredCitiesList.map((city, i) => (
-            <li className="locations__item" key={i}>
-              <a
-                className={`locations__item-link tabs__item ${i === 0 && `tabs__item--active`}`}
-                href="#"
-              >
-                <span>{city.name}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
-  </>;
-};
+    this.activeTab = 0;
+    this.filteredCitiesList = [];
+
+    this.offers = this.props.offers;
+    this.setActiveCity = this.props.setActiveCity;
+    this.setActiveOffers = this.props.setActiveOffers;
+  }
+
+  componentDidMount() {
+    const activeCity = this.filteredCitiesList[this.activeTab];
+
+    this.setActiveCity(activeCity);
+    this._setActiveOffers(activeCity);
+  }
+
+  _filterCities() {
+    const tempCitiesArr = [];
+    this.offers.map((offer) => tempCitiesArr.push(offer.city));
+    this.filteredCitiesList = [...new Set(tempCitiesArr.map(JSON.stringify))].map(JSON.parse);
+  }
+
+  _setActiveOffers(city) {
+    const cityOffers = this.offers.filter((it) => (it.city.name === city.name));
+    this.setActiveOffers(cityOffers);
+  }
+
+  handleClick(city, index) {
+    this.activeTab = index;
+    this.setActiveCity(city);
+    this._setActiveOffers(city);
+  }
+
+  render() {
+    this._filterCities();
+
+    return <>
+      <div className="tabs">
+        <section className="locations container">
+          <ul className="locations__list tabs__list">
+            {this.filteredCitiesList.map((city, i) => (
+              <li className="locations__item" key={i}>
+                <a
+                  className={`locations__item-link tabs__item ${i === this.activeTab && `tabs__item--active`}`}
+                  href="#"
+                  onClick={() => this.handleClick(city, i)}
+                >
+                  <span>{city.name}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </>;
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  setActiveCity: (city) => dispatch(ActionCreator.setCity(city)),
+  setActiveOffers: (offers) => dispatch(ActionCreator.setOffers(offers))
+});
+
 
 CitiesList.propTypes = {
-  offers: PropTypes.arrayOf(offerPropTypes).isRequired
+  offers: PropTypes.arrayOf(offerPropTypes).isRequired,
+  setActiveCity: PropTypes.func.isRequired,
+  setActiveOffers: PropTypes.func.isRequired
 };
 
-export default CitiesList;
+export default connect(null, mapDispatchToProps)(CitiesList);
