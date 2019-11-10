5e0d74f186df863cb01da2814027b7277235c8ab
Funky-Juice <isa86box@gmail.com>
2019-11-10 14:21:39 +0300

    тест на PageHeader

---
+++ src/components/page-header/page-header.test.js
@@ -0,0 +1,11 @@
+import renderer from 'react-test-renderer';
+import PageHeader from './page-header';
+
+
+it(`PageHeader correctly renders`, () => {
+  const tree = renderer
+    .create(<PageHeader/>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
