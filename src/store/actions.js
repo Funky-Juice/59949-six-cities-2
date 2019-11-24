824b6a01c7e726fabafbd99de99f4d50a164b721
Funky-Juice <isa86box@gmail.com>
2019-11-24 12:52:36 +0300

    action'ы загрузки списка предложений аренды и сохранения их в store

--- src/store/actions.js
+++ src/store/actions.js
@@ -14,7 +14,21 @@ const ActionCreator = {
   setActiveOfferId: (payload = null) => ({
     type: types.SET_ACTIVE_OFFER_ID,
     payload
-  })
+  }),
+
+  getOffers: () => (dispatch, _getState, api) => {
+    return api.get(`/hotels`)
+      .then((response) => {
+        dispatch(ActionCreator.setOffers(response.data));
+      });
+  },
+
+  setOffers: (payload = []) => {
+    return {
+      type: types.SET_OFFERS,
+      payload,
+    };
+  }
 };
 
 export default ActionCreator;
