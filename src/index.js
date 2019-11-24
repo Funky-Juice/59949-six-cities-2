0178b21008cd3d52e65515834cd9a45739e51ddf
Funky-Juice <isa86box@gmail.com>
2019-11-24 13:11:20 +0300

    загрузка с сервера предложений по аренде при инициализации приложения

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import ActionCreator from './store/actions';
 import store from './store/store';
 
-import App from './components/app/app';
-import {offers} from './mocks/offers';
+import App from './components/app';
 
-const init = (data) => {
+const init = () => {
+  store.dispatch(ActionCreator.getOffers());
+
   ReactDOM.render(<Provider store={store}>
-    <App offers={data}/>
+    <App/>
   </Provider>,
   document.getElementById(`root`)
   );
 };
 
-init(offers);
+init();
