87d34b5a78d86808053554557446cd37cb459d78
Funky-Juice <isa86box@gmail.com>
2019-11-26 19:49:19 +0300

    вызов action'а requireAuthorization в перехватчике ошибок

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import ActionCreator from '../store/actions';
 
 const apiURL = `https://htmlacademy-react-2.appspot.com/six-cities`;
 
-export const createAPI = () => {
+export const createAPI = (dispatch) => {
   const api = axios.create({
     baseURL: apiURL,
     timeout: 5000,
@@ -12,6 +13,9 @@ export const createAPI = () => {
   const onSuccess = (res) => res;
 
   const onFail = (err) => {
+    if (err.response.status === 403) {
+      dispatch(ActionCreator.requireAuthorization(true));
+    }
     throw err;
   };
 
