c09d0d3f42d2f1815d6edefdf496534f5168b9c3
Funky-Juice <isa86box@gmail.com>
2019-11-28 12:34:31 +0300

    переключение показа меню сортировки по клику

--- src/components/offers-list-sort/offers-list-sort.jsx
+++ src/components/offers-list-sort/offers-list-sort.jsx
@@ -1,29 +1,61 @@
-const OffersListSort = () => {
-  return <>
-    <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
 
-      <span className="places__sorting-type" tabIndex="0">Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
-          <use xlinkHref="#icon-arrow-select"></use>
-        </svg>
-      </span>
+class OffersListSort extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-      <ul className="places__options places__options--custom places__options--opened">
-        <li className="places__option places__option--active" tabIndex="0">Popular</li>
-        <li className="places__option" tabIndex="0">Price: low to high</li>
-        <li className="places__option" tabIndex="0">Price: high to low</li>
-        <li className="places__option" tabIndex="0">Top rated first</li>
-      </ul>
+    this.state = {
+      showSortList: false
+    };
 
-      <select className="places__sorting-type" id="places-sorting">
-        <option className="places__option" value="popular" selected="">Popular</option>
-        <option className="places__option" value="to-high">Price: low to high</option>
-        <option className="places__option" value="to-low">Price: high to low</option>
-        <option className="places__option" value="top-rated">Top rated first</option>
-      </select>
-    </form>
-  </>;
-};
+    this._sortByClickHandler = this._sortByClickHandler.bind(this);
+  }
+
+  _sortByClickHandler() {
+    this.setState((prevState) => ({
+      showSortList: !prevState.showSortList
+    }));
+  }
+
+  _renderSortForm() {
+    return <>
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+
+        <span className="places__sorting-type" tabIndex="0" onClick={this._sortByClickHandler}>
+          Popular
+          <svg
+            className="places__sorting-arrow"
+            style={{transform: `rotateX(${ this.state.showSortList ? `180` : `0`}deg)`}}
+            width="7"
+            height="4"
+          >
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+
+        <ul
+          className="places__options places__options--custom places__options--opened"
+          style={{display: `${ this.state.showSortList ? `block` : `none`}`}}
+        >
+          <li className="places__option places__option--active" tabIndex="0">Popular</li>
+          <li className="places__option" tabIndex="0">Price: low to high</li>
+          <li className="places__option" tabIndex="0">Price: high to low</li>
+          <li className="places__option" tabIndex="0">Top rated first</li>
+        </ul>
+
+        <select className="places__sorting-type" id="places-sorting" style={{display: `none`}}>
+          <option className="places__option" value="popular" selected="">Popular</option>
+          <option className="places__option" value="to-high">Price: low to high</option>
+          <option className="places__option" value="to-low">Price: high to low</option>
+          <option className="places__option" value="top-rated">Top rated first</option>
+        </select>
+      </form>
+    </>;
+  }
+
+  render() {
+    return this._renderSortForm();
+  }
+}
 
 export default OffersListSort;
