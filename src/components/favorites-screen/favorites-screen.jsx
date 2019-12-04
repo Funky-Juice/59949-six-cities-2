329505cc6c6d94c975e742dcb357425d170d7dff
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-12-04 15:08:37 +0300

    создал компонент FavoritesList(статика) и подключил в FavoritesScreen

--- src/components/favorites-screen/favorites-screen.jsx
+++ src/components/favorites-screen/favorites-screen.jsx
@@ -1,5 +1,17 @@
+import FavoritesList from '../favorites-list/favorites-list';
+
 const FavoritesScreen = () => {
-  return <h1 className="favorites__title">Saved listing</h1>;
+  return <>
+    <main className="page__main page__main--favorites">
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+
+          <FavoritesList/>
+        </section>
+      </div>
+    </main>
+  </>;
 };
 
 export default FavoritesScreen;
