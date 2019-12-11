07d3cd713337e0a12c13bb38c163936a2db78185
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-12-11 19:02:41 +0300

    вынес поля рейтинга в массив

--- src/components/reviews-form/reviews-form.jsx
+++ src/components/reviews-form/reviews-form.jsx
@@ -1,44 +1,39 @@
+import {Fragment} from 'react';
 
 const ReviewsForm = () => {
+  const ratingInputs = [
+    {id: 5, value: `perfect`},
+    {id: 4, value: `good`},
+    {id: 3, value: `not bad`},
+    {id: 2, value: `badly`},
+    {id: 1, value: `terribly`}
+  ];
+
   return <>
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
 
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"></input>
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"></input>
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"></input>
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"></input>
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"></input>
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {ratingInputs.map((it) =>
+          <Fragment key={it.id}>
+            <input
+              className="form__rating-input visually-hidden"
+              name="rating"
+              value={it.id}
+              id={`${it.id}-stars`}
+              type="radio"
+            ></input>
+            <label
+              htmlFor={`${it.id}-stars`}
+              className="reviews__rating-label form__rating-label"
+              title={it.value}
+            >
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star"></use>
+              </svg>
+            </label>
+          </Fragment>
+        )}
       </div>
 
       <textarea className="reviews__textarea form__textarea" id="review" name="review"
