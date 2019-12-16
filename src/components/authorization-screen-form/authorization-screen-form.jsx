2243edce30d4881f4c2db31d629037fcccdffed2
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-12-16 19:19:27 +0300

    переписал форму авторизации как функциональный компонент

--- src/components/authorization-screen-form/authorization-screen-form.jsx
+++ src/components/authorization-screen-form/authorization-screen-form.jsx
@@ -1,64 +1,54 @@
 
-class AuthorizationScreenForm extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const AuthorizationScreenForm = (props) => {
+  const {message, isLocked, onFormSubmit} = props;
 
-  _renderAuthForm() {
-    const {message, isLocked, onFormSubmit} = this.props;
+  return <>
+    <section className="login">
+      <h1 className="login__title">Sign in</h1>
 
-    return <>
-      <section className="login">
-        <h1 className="login__title">Sign in</h1>
+      <form
+        className="login__form form"
+        onSubmit={onFormSubmit}
+      >
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">E-mail</label>
+          <input
+            className="login__input form__input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required=""
+          ></input>
+        </div>
 
-        <form
-          className="login__form form"
-          onSubmit={onFormSubmit}
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">Password</label>
+          <input
+            className="login__input form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required=""
+          ></input>
+        </div>
+
+        <button
+          className="login__submit form__submit button"
+          type="submit"
+          disabled={isLocked}
         >
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">E-mail</label>
-            <input
-              className="login__input form__input"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required=""
-            ></input>
-          </div>
+          Sign in
+        </button>
 
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">Password</label>
-            <input
-              className="login__input form__input"
-              type="password"
-              name="password"
-              placeholder="Password"
-              required=""
-            ></input>
-          </div>
-
-          <button
-            className="login__submit form__submit button"
-            type="submit"
-            disabled={isLocked}
-          >
-            Sign in
-          </button>
-
-          {message && <div style={{position: `relative`, fontSize: `14px`, color: `red`}}>
-            <p className="login__error-message" style={{position: `absolute`}}>
-              {message}
-            </p>
-          </div>}
-        </form>
-      </section>
-    </>;
-  }
-
-  render() {
-    return this._renderAuthForm();
-  }
-}
+        {message && <div style={{position: `relative`, fontSize: `14px`, color: `red`}}>
+          <p className="login__error-message" style={{position: `absolute`}}>
+            {message}
+          </p>
+        </div>}
+      </form>
+    </section>
+  </>;
+};
 
 AuthorizationScreenForm.propTypes = {
   message: PropTypes.string,
