72e4a7e59ff6f97381a2378a945eece64a5aaaaf
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-12-26 19:11:54 +0300

    создал интерфейсы для валидации пропсов

---
+++ src/types/interfaces.ts
@@ -0,0 +1,64 @@
+export enum Type {
+    APARTMENT = `apartment`,
+    ROOM = `room`,
+    HOUSE = `house`,
+    HOTEL = `hotel`
+}
+
+export interface Offer {
+    id: number
+    type: Type
+    price: number
+    title: string
+    preview_image: string
+    images: string[]
+    goods: string[]
+    rating: number
+    is_premium: boolean
+    is_favorite: boolean
+    description: string
+    city: City
+    host: Host
+    location: {
+        latitude: number
+        longitude: number
+    }
+}
+
+export interface City {
+    name: string
+    location: Location
+}
+
+export interface Location {
+    latitude: number
+    longitude: number
+    zoom: number
+}
+
+export interface Host {
+    name: string
+    status: string
+    avatar_url: string
+}
+
+export interface User {
+    id: number
+    email: string
+    name: string
+    avatar_url: string
+    is_pro: boolean
+}
+
+export interface ActiveCity {
+    name: string
+    location: Location
+}
+
+export interface Review {
+    id: number
+    date: string
+    rating: number
+    comment: string
+    user: User
+}
