6a553a6cb4697bc6066751bb6f18f8d9b17a7002
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-12-30 11:02:33 +0300

    дополнил конифг Jest'а

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   verbose: true,
   setupFiles: [`./jest.setup.js`],
+  transform: {
+    '^.+\\.tsx?$': `ts-jest`,
+    '^.+\\.js?$': `babel-jest`,
+  },
+  testRegex: `.test.(js?|jsx?|tsx?)$`,
+  moduleFileExtensions: [
+    `ts`,
+    `tsx`,
+    `js`,
+    `jsx`,
+    `json`,
+    `node`
+  ],
 };
